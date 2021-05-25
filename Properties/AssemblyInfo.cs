8c93f62c7a1594f88d391b28b75b36055b3266ea
wy-luke <li1078797083@qq.com>
2021-05-25 21:46:01 +0800

    增加一些设置信息

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Windows;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("CopyPlusPlus")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Make copy more easily!")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("CopyPlusPlus")]
 [assembly: AssemblyProduct("CopyPlusPlus")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]

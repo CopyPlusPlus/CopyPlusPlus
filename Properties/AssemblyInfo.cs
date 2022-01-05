20744d6f244777f9687465017311606ca29a8079
wy-luke <li1078797083@qq.com>
2022-01-05 10:33:49 +0800

    更改文件信息

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Windows;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("CopyPlusPlus")]
-[assembly: AssemblyDescription("Make copy more easily!")]
+[assembly: AssemblyDescription("让复制变得更加简单！")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("CopyPlusPlus")]
 [assembly: AssemblyProduct("CopyPlusPlus")]

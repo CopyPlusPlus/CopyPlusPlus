a123d3e1cdf1678931b819ec84034fa86879a18e
wy-luke <li1078797083@qq.com>
2022-01-06 16:50:10 +0800

    加入图标弹窗自动消失

--- IconPopup.xaml.cs
+++ IconPopup.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
+using System.Timers;
 using System.Windows;
+using System.Windows.Input;
 
 namespace CopyPlusPlus
 {
@@ -8,21 +11,63 @@ namespace CopyPlusPlus
     /// </summary>
     public partial class IconPopup
     {
-        public string CopiedText;
+        private static Timer _aTimer;
 
         //Get MainWindow
         private readonly MainWindow _mainWindow = Application.Current.Windows
             .Cast<Window>()
             .FirstOrDefault(window => window is MainWindow) as MainWindow;
 
+        private double _opacity = 1;
+
+        public string CopiedText;
+
         public IconPopup()
         {
             InitializeComponent();
+
+            Fade();
         }
 
-        private void OnCopyClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        private void OnCopyClick(object sender, MouseButtonEventArgs e)
         {
             _mainWindow.ProcessText(CopiedText);
+        }
+
+        private void Fade()
+        {
+            // Create a timer with a two second interval.
+            _aTimer = new Timer(100);
+            // Hook up the Elapsed event for the timer.
+            _aTimer.Elapsed += OnTimedEvent;
+            _aTimer.AutoReset = true;
+            _aTimer.Enabled = true;
+        }
+
+        private void OnTimedEvent(object source, ElapsedEventArgs e)
+        {
+            _opacity -= 0.04;
+            if (_opacity == 0) Close();
+
+            Icon.Dispatcher.Invoke(delegate { Icon.Opacity = _opacity; });
+        }
+
+        private void Icon_OnMouseEnter(object sender, MouseEventArgs e)
+        {
+            Icon.Dispatcher.Invoke(delegate { Icon.Opacity = 1; });
+            _aTimer.Stop();
+            _opacity = 1;
+        }
+
+        private void Icon_OnMouseLeave(object sender, MouseEventArgs e)
+        {
+            _aTimer.Start();
+        }
+
+        private void OnClosed(object sender, EventArgs e)
+        {
+            _aTimer.Stop();
+            _aTimer.Dispose();
         }
     }
 }

ebdf455b5deac6e3cdd3f1c9b98f981114b51b19
wy-luke <li1078797083@qq.com>
2022-01-06 18:09:06 +0800

    图标弹窗停留两秒再消失

--- IconPopup.xaml.cs
+++ IconPopup.xaml.cs
@@ -11,7 +11,8 @@ namespace CopyPlusPlus
     /// </summary>
     public partial class IconPopup
     {
-        private static Timer _aTimer;
+        private static Timer _fadeTimer;
+        private static Timer _stayTimer;
 
         //Get MainWindow
         private readonly MainWindow _mainWindow = Application.Current.Windows
@@ -26,7 +27,18 @@ namespace CopyPlusPlus
         {
             InitializeComponent();
 
-            Fade();
+            // Create a timer
+            _stayTimer = new Timer(2000);
+            // Hook up the Elapsed event for the timer.
+            _stayTimer.Elapsed += OnStayTimedEvent;
+
+            // Create a timer
+            _fadeTimer = new Timer(100);
+            // Hook up the Elapsed event for the timer.
+            _fadeTimer.Elapsed += OnFadeTimedEvent;
+            _fadeTimer.AutoReset = true;
+
+            Stay();
         }
 
         private void OnCopyClick(object sender, MouseButtonEventArgs e)
@@ -34,17 +46,23 @@ namespace CopyPlusPlus
             _mainWindow.ProcessText(CopiedText);
         }
 
-        private void Fade()
+        private static void Stay()
         {
-            // Create a timer with a two second interval.
-            _aTimer = new Timer(100);
-            // Hook up the Elapsed event for the timer.
-            _aTimer.Elapsed += OnTimedEvent;
-            _aTimer.AutoReset = true;
-            _aTimer.Enabled = true;
+            _stayTimer.Start();
         }
 
-        private void OnTimedEvent(object source, ElapsedEventArgs e)
+        private static void OnStayTimedEvent(object source, ElapsedEventArgs e)
+        {
+            Fade();
+            _stayTimer.Stop();
+        }
+
+        private static void Fade()
+        {
+            _fadeTimer.Start();
+        }
+
+        private void OnFadeTimedEvent(object source, ElapsedEventArgs e)
         {
             _opacity -= 0.04;
             if (_opacity == 0) Close();
@@ -55,19 +73,24 @@ namespace CopyPlusPlus
         private void Icon_OnMouseEnter(object sender, MouseEventArgs e)
         {
             Icon.Dispatcher.Invoke(delegate { Icon.Opacity = 1; });
-            _aTimer.Stop();
+
+            _stayTimer.Stop();
+            _fadeTimer.Stop();
             _opacity = 1;
         }
 
         private void Icon_OnMouseLeave(object sender, MouseEventArgs e)
         {
-            _aTimer.Start();
+            Stay();
         }
 
         private void OnClosed(object sender, EventArgs e)
         {
-            _aTimer.Stop();
-            _aTimer.Dispose();
+            _fadeTimer.Stop();
+            _fadeTimer.Dispose();
+
+            _stayTimer.Stop();
+            _stayTimer.Dispose();
         }
     }
 }

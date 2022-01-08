8b0fd5faccb74c9ef8b038dcdeb42ada573d6b55
wy-luke <li1078797083@qq.com>
2022-01-08 12:13:51 +0800

    注释翻译结果自动消失

--- TranslateResult.xaml.cs
+++ TranslateResult.xaml.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Linq;
-using System.Timers;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Input;
 using System.Windows.Media;
 
@@ -14,15 +11,15 @@ namespace CopyPlusPlus
     {
         public static RoutedCommand EscEvent = new RoutedCommand();
 
-        private static Timer _fadeTimer;
-        private static Timer _stayTimer;
+        //private static Timer _fadeTimer;
+        //private static Timer _stayTimer;
 
-        //Get MainWindow
-        private readonly MainWindow _mainWindow = Application.Current.Windows
-            .Cast<Window>()
-            .FirstOrDefault(window => window is MainWindow) as MainWindow;
+        ////Get MainWindow
+        //private readonly MainWindow _mainWindow = Application.Current.Windows
+        //    .Cast<Window>()
+        //    .FirstOrDefault(window => window is MainWindow) as MainWindow;
 
-        private double _opacity = 1;
+        //private double _opacity = 1;
 
         public TranslateResult()
         {
@@ -30,18 +27,18 @@ namespace CopyPlusPlus
 
             EscEvent.InputGestures.Add(new KeyGesture(Key.Escape));
 
-            // Create a timer
-            _stayTimer = new Timer(2000);
-            // Hook up the Elapsed event for the timer.
-            _stayTimer.Elapsed += OnStayTimedEvent;
+            //// Create a timer
+            //_stayTimer = new Timer(2000);
+            //// Hook up the Elapsed event for the timer.
+            //_stayTimer.Elapsed += OnStayTimedEvent;
 
-            // Create a timer
-            _fadeTimer = new Timer(100);
-            // Hook up the Elapsed event for the timer.
-            _fadeTimer.Elapsed += OnFadeTimedEvent;
-            _fadeTimer.AutoReset = true;
+            //// Create a timer
+            //_fadeTimer = new Timer(100);
+            //// Hook up the Elapsed event for the timer.
+            //_fadeTimer.Elapsed += OnFadeTimedEvent;
+            //_fadeTimer.AutoReset = true;
 
-            Stay();
+            //Stay();
         }
 
         private void EscExecuted(object sender, ExecutedRoutedEventArgs e)
@@ -58,51 +55,51 @@ namespace CopyPlusPlus
                 : (Brush)converter.ConvertFromString("#FFFFFEFF");
         }
 
-        private static void Stay()
-        {
-            _stayTimer.Start();
-        }
+        //private static void Stay()
+        //{
+        //    _stayTimer.Start();
+        //}
 
-        private static void OnStayTimedEvent(object source, ElapsedEventArgs e)
-        {
-            Fade();
-            _stayTimer.Stop();
-        }
+        //private static void OnStayTimedEvent(object source, ElapsedEventArgs e)
+        //{
+        //    Fade();
+        //    _stayTimer.Stop();
+        //}
 
-        private static void Fade()
-        {
-            _fadeTimer.Start();
-        }
+        //private static void Fade()
+        //{
+        //    _fadeTimer.Start();
+        //}
 
-        private void OnFadeTimedEvent(object source, ElapsedEventArgs e)
-        {
-            _opacity -= 0.04;
-            if (_opacity == 0) Close();
+        //private void OnFadeTimedEvent(object source, ElapsedEventArgs e)
+        //{
+        //    _opacity -= 0.04;
+        //    if (_opacity == 0) Close();
 
-            Dispatcher.Invoke(delegate { Opacity = _opacity; });
-        }
+        //    Dispatcher.Invoke(delegate { Opacity = _opacity; });
+        //}
 
-        private void OnMouseEnter(object sender, MouseEventArgs e)
-        {
-            Dispatcher.Invoke(delegate { Opacity = 1; });
+        //private void OnMouseEnter(object sender, MouseEventArgs e)
+        //{
+        //    Dispatcher.Invoke(delegate { Opacity = 1; });
 
-            _stayTimer.Stop();
-            _fadeTimer.Stop();
-            _opacity = 1;
-        }
+        //    _stayTimer.Stop();
+        //    _fadeTimer.Stop();
+        //    _opacity = 1;
+        //}
 
-        private void OnMouseLeave(object sender, MouseEventArgs e)
-        {
-            Stay();
-        }
+        //private void OnMouseLeave(object sender, MouseEventArgs e)
+        //{
+        //    Stay();
+        //}
 
-        private void OnClosed(object sender, EventArgs e)
-        {
-            _fadeTimer.Stop();
-            _fadeTimer.Dispose();
+        //private void OnClosed(object sender, EventArgs e)
+        //{
+        //    _fadeTimer.Stop();
+        //    _fadeTimer.Dispose();
 
-            _stayTimer.Stop();
-            _stayTimer.Dispose();
-        }
+        //    _stayTimer.Stop();
+        //    _stayTimer.Dispose();
+        //}
     }
 }
